7a9937ee61e66028d9a5da0080ed4eca251d51b7
Dries007 <admin@dries007.net>
2015-01-03 16:55:07 +0100

    made some links open in new tabs, as sugested

--- assets/pages/home.php
+++ assets/pages/home.php
@@ -1,12 +1,12 @@
 <h2 class="hiddenlink"><a href="?p=modlist">Our mods</a></h2>
 <h2 class="hiddenlink"><a href="?p=projects">Our other projects</a></h2>
 <br>
-<h2 class="hiddenlink"><a href="https://github.com/orgs/DoubleDoorDevelopment/">Github: DoubleDoorDevelopment</a></h2>
-<h2 class="hiddenlink"><a href="https://twitter.com/DoubleDoorDev">Twitter: @DoubleDoorDev</a></h2>
-<h2 class="hiddenlink"><a href="irc://irc.esper.net:6697/#D3">IRC: #D3 on Esper.net</a></h2>
-<h2 class="hiddenlink"><a href="ts3server://ts.doubledoordev.net/">Teamspeak: ts.doubledoordev.net</a></h2>
+<h2 class="hiddenlink"><a href="https://github.com/orgs/DoubleDoorDevelopment/" target="_blank">Github: DoubleDoorDevelopment</a></h2>
+<h2 class="hiddenlink"><a href="https://twitter.com/DoubleDoorDev" target="_blank">Twitter: @DoubleDoorDev</a></h2>
+<h2 class="hiddenlink"><a href="irc://irc.esper.net:6697/#D3" target="_blank">IRC: #D3 on Esper.net</a></h2>
+<h2 class="hiddenlink"><a href="ts3server://ts.doubledoordev.net/" target="_blank">Teamspeak: ts.doubledoordev.net</a></h2>
 <h2 class="hiddenlink"><a href="?p=contact">Other contact info</a></h2>
 <br>
-<h2 class="hiddenlink"><a href="http://jenkins.dries007.net">Our build server</a></h2>
-<h2 class="hiddenlink"><a href="maven">Our maven repo <span class="small">(for gradle)</span></a></h2>
-<h2 class="hiddenlink"><a href="artifactory">Our maven interface <span class="small">(for humans)</span></a></h2>
+<h2 class="hiddenlink"><a href="http://jenkins.dries007.net" target="_blank">Our build server</a></h2>
+<h2 class="hiddenlink"><a href="maven" target="_blank">Our maven repo <span class="small">(for gradle)</span></a></h2>
+<h2 class="hiddenlink"><a href="artifactory" target="_blank">Our maven interface <span class="small">(for humans)</span></a></h2>
