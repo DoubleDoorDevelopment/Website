eb34b20e9f046160898ca80b4b5ae97dcf47ed84
Dries007 <admin@dries007.net>
2015-01-03 16:47:37 +0100

    fixed build server link

--- assets/pages/home.php
+++ assets/pages/home.php
@@ -7,6 +7,6 @@
 <h2 class="hiddenlink"><a href="ts3server://ts.doubledoordev.net/">Teamspeak: ts.doubledoordev.net</a></h2>
 <h2 class="hiddenlink"><a href="?p=contact">Other contact info</a></h2>
 <br>
-<h2 class="hiddenlink"><a href="http://jenkins.dries007.net/view/DoubleDoorDevelopment/">Our build server</a></h2>
+<h2 class="hiddenlink"><a href="http://jenkins.dries007.net">Our build server</a></h2>
 <h2 class="hiddenlink"><a href="maven">Our maven repo <span class="small">(for gradle)</span></a></h2>
 <h2 class="hiddenlink"><a href="artifactory">Our maven interface <span class="small">(for humans)</span></a></h2>
