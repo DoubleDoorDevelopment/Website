1d1e6020bc38dd5d8f625f1ed677f47a12167b9f
Dries007 <admin@dries007.net>
2015-01-27 20:17:43 +0100

    all of the data!

--- index.php
+++ index.php
@@ -65,6 +65,7 @@
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow' rel='stylesheet' type='text/css'>
     <link href="assets/css/style.css" rel="stylesheet" type='text/css'>
+    <meta name="google-site-verification" content="pVuXOM2EbuoENke7yMvrXbC7NUNzJUNLQ3hC7XGYnrs" />
   </head>
   <body>
     <div class="container container-narrow" id="wrap">
@@ -88,5 +89,15 @@
 		</div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-59021772-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
   </body>
 </html>
